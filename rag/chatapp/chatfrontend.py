40111fcb8a5428c226167608715465704f87646b
ocbc01 <iisuru@gmail.com>
2023-12-04 17:36:17 +0530

    addconversational

--- rag/chatapp/chatfrontend.py
+++ rag/chatapp/chatfrontend.py
@@ -12,6 +12,8 @@ if "user_prompt_history" not in st.session_state:
 
 if "chat_answer_history" not in st.session_state:
         st.session_state["chat_answer_history"] = []
+if "chat_history" not in st.session_state:
+        st.session_state["chat_history"] = []
 def create_sources_string(source_urls: Set[str])-> str:
     if not source_urls:
         return ""
@@ -23,11 +25,12 @@ def create_sources_string(source_urls: Set[str])-> str:
     return sources_string
 if prompt:
     with st.spinner("Generating response...."):
-        generated_response = run_llm(query=prompt)
+        generated_response = run_llm(query=prompt,chat_history=st.session_state["chat_history"])
         sources = set([doc.metadata["source"] for doc in generated_response["source_documents"]])
-        formatted_response = (f"{generated_response['result']} \n\n {create_sources_string(sources)}")
+        formatted_response = (f"{generated_response['answer']} \n\n {create_sources_string(sources)}")
         st.session_state["user_prompt_history"].append(prompt)
         st.session_state["chat_answer_history"].append(formatted_response)
+        st.session_state["chat_history"].append((prompt,generated_response["answer"]))
 
 if st.session_state["chat_answer_history"]:
     for generated_response, user_query in zip(st.session_state["chat_answer_history"],st.session_state["user_prompt_history"]):
--- rag/chatapp/retrieval.py
+++ rag/chatapp/retrieval.py
@@ -1,23 +1,30 @@
 import os
+from typing import List, Any, Dict
 
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
-from langchain.chains import RetrievalQA
+#from langchain.chains import RetrievalQA
+from langchain.chains import ConversationalRetrievalChain
 from langchain.vectorstores import Pinecone
 from langchain import OpenAI
 
 import os
 import pinecone
+
+
 pinecone.init(api_key="08ca8063-1c86-4ac3-935c-c4242a0ab274",environment="gcp-starter")
 
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
-def run_llm(query:str)-> any:
+def run_llm(query:str, chat_history:List[Dict[str,Any]]=[])-> any:
     embeddings=OpenAIEmbeddings()
     docsearch = Pinecone.from_existing_index(index_name="medium-blog-embedding-index",embedding=embeddings)
     chat=ChatOpenAI(verbose=True,temperature=0.0)
-    qa=RetrievalQA.from_chain_type(llm=chat,chain_type="stuff",retriever=docsearch.as_retriever(),return_source_documents=True)
-    return qa({"query":query})
+    #qa=RetrievalQA.from_chain_type(llm=chat,chain_type="stuff",retriever=docsearch.as_retriever(),return_source_documents=True)
+    qa=ConversationalRetrievalChain.from_llm(llm=chat,retriever=docsearch.as_retriever(),return_source_documents=True)
+    #  return qa({"query":query,"chat_history": chat_history}
+    return qa({"question":query,"chat_history": chat_history})
+
 
 if __name__=="__main__":
     print(run_llm(query="What is LangChain"))
